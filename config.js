42f907b0a3f6491a26c8b2f7836c5fcfee5b5ab8
gsa-official <spicy@therealgs.co.uk>
2025-08-08 21:06:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://spicy:rfyzxtSw58xq@cluster0.8jlmzhj.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
